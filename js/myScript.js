415d11d8754170710bc8e5d0fec868ebf06a5d60
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-04-19 18:16:18 +0200

    Coupon

--- js/myScript.js
+++ js/myScript.js
@@ -4,14 +4,17 @@ window.addEventListener("load", function (event) {
 });
 
 function onWindowLoad() {
+    var sconto =
     ingredients = document.querySelectorAll("[type=checkbox]");
     console.log(ingredients);
     var button = document.getElementById("button");
+    coupon = document.getElementById("coupon");
     button.addEventListener("click", function (event) {
-        var price = parseInt(document.getElementById("price"));
-        document.getElementById("price").innerHTML = calculatePrice(5).toFixed(2);
+        var userCoupon = document.getElementById("coupon").value;
+        document.getElementById("price").innerHTML = (calculatePrice(5) - (5 * calculateCoupon(userCoupon) )).toFixed(2);
     });
 }
+
 function calculatePrice(burgerPrice) {
     ingredients = document.querySelectorAll("[type=checkbox]");
     var somma = burgerPrice;
@@ -22,4 +25,30 @@ function calculatePrice(burgerPrice) {
         }
     }
     return somma;
+}
+
+function calculateCoupon(couponToValidate) {
+    if (couponToValidate == "") {
+        return 1;
+    }
+    else {
+
+        var couponArray = ["12354ABCDEF", "SCONTO10", "1234"];
+        var sconto = 1;
+        var saleHTML = document.getElementById("sale-status");
+        for (var i = 0; i < couponArray.length; i++) {
+            if (couponToValidate == couponArray[i]) {
+                console.log("SCONTO TROVATO!");
+                sconto = 0.2;
+                saleHTML.innerHTML = "IL TUO COUPON E' STATO APPLICATO";
+                saleHTML.style = "color : #8cc371"
+            }
+            else {
+                console.log("SCONTO NON TROVATO!");
+                saleHTML.innerHTML = "IL TUO COUPON NON E' VALIDO";
+                saleHTML.style = "color : red"
+            }
+        }
+        return sconto;
+    }
 }
